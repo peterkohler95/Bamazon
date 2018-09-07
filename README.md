22edf4c8772bdc39c5c0338a615ae55bc9a726f0
Peter K <peterkohler95@gmail.com>
2018-09-06 22:44:12 -0500

    edited console.log statements

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Bamazon
+
+Bamazon is a Amazon-like storefront with MySQL. The app will take in orders from customers and deplete stock from the store's inventory.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -59,38 +59,28 @@ function promptFunc() {
             message: 'How many do you need?'
         }
     ]).then(function (input) {
-        // console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
+
 
         var item = input.item_id;
         var quantity = input.quantity;
 
-        // Query db to confirm that the given item ID exists in the desired quantity
         var queryStr = 'SELECT * FROM products WHERE ?';
 
         connection.query(queryStr, { item_id: item }, function (err, data) {
             if (err) throw err;
 
-            // If the user has selected an invalid item ID, data attay will be empty
-            // console.log('data = ' + JSON.stringify(data));
-
             if (data.length === 0) {
-                console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+                console.log('Please select a valid Item ID.');
                 displayInventory();
 
             } else {
                 var productData = data[0];
 
-                // console.log('productData = ' + JSON.stringify(productData));
-                // console.log('productData.stock_quantity = ' + productData.stock_quantity);
-
-                // If the quantity requested by the user is in stock
                 if (quantity <= productData.stock_quantity) {
                     console.log('Congratulations, the product you requested is in stock! Placing order!');
 
-                    // Construct the updating query string
                     var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
 
-                    // Update the inventory
                     connection.query(updateQueryStr, function (err, data) {
                         if (err) throw err;
 
@@ -98,7 +88,6 @@ function promptFunc() {
 
                         console.log("\n---------------------------------------------------------------------\n");
 
-                        // End the database connection
                         connection.end();
                     })
                 } else {
